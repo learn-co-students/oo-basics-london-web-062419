7f92bf26be73389e80f9405b7fb6605f757775c5
slumfrog <kishan.anil@gmail.com>
2019-06-26 21:07:33 +0100

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,4 +1,8 @@
 class Book
+
+    attr_accessor :author, :page_count, :genre #all setters and getters are being replaced by this current line
+
+
     def initialize(title = "And Then There Were None")
         @title = title
     end
@@ -7,29 +11,29 @@ class Book
         @title
     end
 
-    def author=(author)
-        @author = author
-    end
+    # def author=(author)
+    #     @author = author
+    # end
 
-    def author
-        @author
-    end
+    # def author
+    #     @author
+    # end
 
-    def page_count=(page_count)
-        @page_count = page_count
-    end
+    # def page_count=(page_count)
+    #     @page_count = page_count
+    # end
 
-    def page_count
-        @page_count
-    end
+    # def page_count
+    #     @page_count
+    # end
 
-    def genre=(genre)
-        @genre = genre
-    end
+    # def genre=(genre)
+    #     @genre = genre
+    # end
 
-    def genre
-        @genre
-    end
+    # def genre
+    #     @genre
+    # end
 
     def turn_page
         puts "Flipping the page...wow, you read fast!"
